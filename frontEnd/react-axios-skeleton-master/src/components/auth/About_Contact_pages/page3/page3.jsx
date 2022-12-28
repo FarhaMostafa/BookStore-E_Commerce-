5372bcbd446f50d071a73f98174a850940ccc66c
Farha Mostafa <93776451+FarhaMostafa@users.noreply.github.com>
2022-12-28 15:40:29 +0200

    add front

--- frontEnd/react-axios-skeleton-master/src/components/auth/About_Contact_pages/page3/page3.jsx
+++ frontEnd/react-axios-skeleton-master/src/components/auth/About_Contact_pages/page3/page3.jsx
@@ -18,22 +18,20 @@ const page3 = () => {
         <>
             <AppBar position="static">
                 <Toolbar>
-                    <b className='title'>BookSky</b>
-                    <Toolbar />
-                    <Link href={ROUTES.page6} variant="body2">
-                        <Button >Novels</Button>
+                    <Typography variant='h6' component='div' sx={{ flexGrow: 1 }} style={{ flex: 1 }}>
+                        BookSky store
+                    </Typography>
+                    <Link href={ROUTES.Contact_us} variant="body2">
+                        <Button variant="text" color="success" >contactus</Button>
                     </Link>
-                    <Link href={ROUTES.page2} >
-                        <Button >Literature</Button>
+                    <Link href={ROUTES.About_us} variant="body2">
+                        <Button variant="text" color="success" >Aboutus</Button>
                     </Link>
-                    <Link href={ROUTES.Historical} >
-                        <Button >Historical Book</Button>
+                    <Link href={ROUTES.SIGN_IN} variant="body2">
+                        <Button variant="text" color="success">Signin</Button>
                     </Link>
-                    <Link href={ROUTES.Scientific} >
-                        <Button >Scientific Book</Button>
-                    </Link>
-                    <Link href={ROUTES.Languages} >
-                        <Button >Languages</Button>
+                    <Link href={ROUTES.SIGN_UP} variant="body2">
+                        <Button variant="text" color="success">register</Button>
                     </Link>
                 </Toolbar>
             </AppBar>
@@ -57,6 +55,50 @@ const page3 = () => {
                     ))}
                 </Grid>
             </Container>
+            <div style={{
+                background: "#848482",
+                padding: "20px",
+                margin: "5px",
+            }}>
+                <Container maxWidth="9g" style={{ marginTop: "10px" }}>
+                    <Typography variant='h6'
+                        align="left"
+                        color='textPrimary'
+                        gutterBottom>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td>instractions</td>
+                                    <td>Company</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><Link href={ROUTES.Contact_us} variant="body2" color='inherit'>Terms of service</Link></td>
+                                    <td><Link href={ROUTES.Contact_us} variant="body2" color='inherit'>contact us</Link></td>
+                                </tr>
+                                <tr>
+                                    <td><Link href={ROUTES.About_us} variant="body2" color='inherit'>Copyright </Link></td>
+                                    <td><Link href={ROUTES.About_us} variant="body2" color='inherit'>About us</Link></td>
+                                </tr>
+                                <tr>
+                                    <td><Link href={ROUTES.About_us} variant="body2" color='inherit'>Return policy</Link></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </Typography>
+                    <div style={{ padding: "20px", margin: "5px" }}>
+                        <Container maxWidth="9g" >
+                            <Typography variant='h7'
+                                align="center"
+                                color='textPrimary'
+                                gutterBottom>
+                                All rights reserved to BookSky 2021-2022 - by our teams
+                            </Typography>
+                        </Container>
+                    </div>
+                </Container>
+            </div>
         </>
     );
 };
